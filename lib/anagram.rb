5962f06dabfb9fbd37a80384803f90a02db9b895
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-14 18:58:39 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 
   def match(anagram_arr)
     anagrams =[]
-    anagram_arr.detect{|anagram| anagrams << anagram if anagram.split("").sort == %w(word).sort}
+    anagram_arr.detect{|gram| anagrams << gram if gram.split("").sort == %w(word).sort}
   end
 
 
